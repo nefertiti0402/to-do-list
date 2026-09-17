b444ad8afc16cff369f2a839f4b88f7341e25039
nefertiti0402 <nacamr@gmail.com>
2026-09-17 15:56:59 +0200

    to do list

---
+++ script.js
@@ -0,0 +1,9 @@
+function createNewTask() {
+    console.log ('Nov zadatak!');
+
+}
+const createTaskBtn=document.querySelector('#createTaskBtn');
+console.log(createTaskBtn)
+
+createTaskBtn.addEventListener ('click ' , createNewTask);
+
